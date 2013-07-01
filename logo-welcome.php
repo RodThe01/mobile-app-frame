9fbb69584c74c1a7cfe2e5027b416c7838fd72f1
Rodney Gwamba <rodney.gwamba@gmail.com>
2013-07-01 14:52:22 +0100

    Added 2 files and stylesheet, to display basic page.

---
+++ logo-welcome.php
@@ -0,0 +1,13 @@
+<div id="logo-page-container" class="ui-icon-alt" id="panel-fixed-page1">
+	<div id="logo-text-container">
+		<h1>
+			<span class="logotext the-logo-text" style="color: black;">
+				THE
+			</span>
+			<span class="logotext rod-logo-text" style="color: red;">
+				ROD
+			</span>
+		</h1>
+	</div>
+</div><!-- /div -->
+
---
+++ panel-wrapper.php
@@ -0,0 +1,5 @@
+	<div data-url="panel-fixed-page1" data-role="page" class="jqm-demos ui-responsive-panel" id="panel-fixed-page1" style="background: #FFFFFF;">
+		<div data-role="content">
+			<?php include 'logo-welcome.php' ?>
+		</div>
+	</div>
