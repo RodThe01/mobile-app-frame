83c49d00fd8e7c3aee2c4201f1a2729911cc7fa9
Rodney Gwamba <rodney.gwamba@gmail.com>
2013-07-01 20:42:53 +0100

    Addition of welcome page with home button as link

---
+++ welcome.php
@@ -0,0 +1,5 @@
+<div class="ui-icon-alt" data-theme="f">
+	<div id="welcome-link">
+		<a href="#signin" data-role="button" data-icon="home" data-iconpos="notext" data-theme="c" data-inline="true">Home</a>
+	</div>
+</div><!-- /div -->
