bcf7b675e5ad4bf4a1f6bd52ec0f7e37f94ca016
Rodney Gwamba <rodney.gwamba@gmail.com>
2013-07-01 13:40:48 +0100

    Initial commit, added index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Mobile App Frame</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
+    <style type="text/css">
+    </style>
+</head>
+<body>
+	<?php include "panel-wrapper.php"; ?>
+</body>
+</html>
